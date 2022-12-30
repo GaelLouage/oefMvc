863d61281f554bc4671b39d588b8fd41071c70c3
GaelLouage <Gael.Louage@hotmail.com>
2022-12-30 12:56:20 +0100

    exercise students

---
+++ Infrastructuur/Files/Data.cs
@@ -0,0 +1,30 @@
+﻿using Infrastructuur.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructuur.Files
+{
+    public class Data
+    {
+     
+        public IEnumerable<StudentEntity> GetStudentsFromFile()
+        {
+            var id = 1;
+            var fileName = Path.Combine(Directory.GetCurrentDirectory(), "Studenten.txt");
+            if (!File.Exists(fileName))
+            {
+                throw new Exception("File not found");
+            }
+            var students = File.ReadAllLines(fileName);
+            for (int i = 0; i < students.Length; i++)
+            {
+                string[] splitted = students[i].Split(',');
+                yield return new StudentEntity() { Id= id, FirstName = splitted[0], LastName = splitted[1], Points = double.Parse(splitted[2]) };
+                id++;
+            }
+        }
+    }
+}
---
+++ OefOpASPMvc/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Infrastructuur.Files;
+using Microsoft.AspNetCore.Mvc;
+using OefOpASPMvc.Models;
+using System.Diagnostics;
+
+namespace OefOpASPMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly Data _studentsData;
+
+
+        public HomeController(ILogger<HomeController> logger, Data studentsData)
+        {
+            _logger = logger;
+            _studentsData = studentsData;
+        }
+
+        public IActionResult Index(string searchInput, string filter)
+        {
+            var students = _studentsData.GetStudentsFromFile().ToList();
+            if (!string.IsNullOrEmpty(searchInput)) 
+            {
+                searchInput = searchInput.ToLower();
+                students = students.Where(x => x.FirstName.ToLower().Contains(searchInput) ||
+                                          x.LastName.ToLower().Contains(searchInput)).ToList();
+            }
+            switch (filter)
+            {
+                case "FirstName":
+                    students = students.OrderBy(x => x.FirstName).ToList();
+                    break;
+                case "LastName":
+                    students = students.OrderBy(x => x.LastName).ToList();
+                    break;
+                case "Points":
+                    students = students.OrderByDescending(x => x.Points).ToList();
+                    break;
+                default:
+                    students = students.OrderBy(x => x.Id).ToList();
+                    break;
+            }
+            return View(students);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
